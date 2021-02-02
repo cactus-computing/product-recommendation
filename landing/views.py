d4b6446e8285569dba16917744c2cf8aa6bb52c1
Agustín Escobar <agustin.escobar.blanc@gmail.com>
2021-02-02 17:07:46 -0300

    added example to landing page plus WHY statement

--- landing/views.py
+++ landing/views.py
@@ -10,7 +10,53 @@ from django.urls import reverse
 from django.template import Context, loader
 
 logger = logging.Logger(__name__)
-
+recommended_products = [
+        {
+            "code": "ROD013373",
+            "img_name": "img/ROD013373.png",
+            "name": "RODILLO CHIPORRO 7 Pul. 18 CMS. LIZCAL",
+            "recommended": [
+                { "name": "RODILLO TEXTURAR 7 Pul. 18 CMS. LIZCAL", "score": 0.9659267 },
+                { "name": "RODILLO ESPONJA 7 Pul. 18 CMS. LIZCAL", "score": 0.9403552 },
+                { "name": "RODILLO CHIPORRO 9 Pul. 24 CMS. LIZCAL", "score": 0.8460233 },
+                { "name": "RODILLO CHIPORRO 4 Pul. 9 CMS LIZCAL", "score": 0.81059957 },
+                { "name": "RODILLO ESPONJA 3 Pul. 7 CMS. LIZCAL", "score": 0.79706407 },
+                { "name": "RODILLO CHIPORRO 5 Pul. 12 CMS. LIZCAL", "score": 0.79489124 },
+                { "name": "RODILLO ESPONJA 1 1/2 Pul. 4.5 CMS. LIZCAL", "score": 0.78571963 },
+                { "name": "RODILLO ESPONJA 5 Pul. 12 CMS. LIZCAL", "score": 0.7753909 }
+            ]
+        },
+        {
+            "code": "PAS119006",
+            "img_name": "img/PAS119006.jpeg",
+            "name": "PASTA MURO TAJAMAR F-15 25 KGS",
+            "recommended": [
+                { "name": "PASTA MURO TAJAMAR F-15 30 KGS", "score": 0.9304087 },
+                { "name": "PASTA MURO TAJAMAR F-15 1/2 TINETA", "score": 0.82496035 },
+                { "name": "SOQUINA PASTA MURO 1KG", "score": 0.6861458 },
+                { "name": "PASTA ESMERIL PULIR 250 GRAMOS GRANO N° 100", "score": 0.6724293 },
+                { "name": "ADHESIVO PASTA CADINA AC BALDE 25 KG", "score": 0.6693083 },
+                { "name": "TAJAMAR PASTA MURO SUPER F-6 1KG", "score": 0.6657156 },
+                { "name": "CERESITA PASTA MURO 1KG", "score": 0.6600415 },
+                { "name": "PASTA ESMERIL PULIR 250 GRAMOS GRANO N° 120", "score": 0.65710866 }
+            ]
+        },
+        {
+            "code": "TER018624",
+            "img_name": "img/TER018624.jpg",
+            "name": "TERCIADO ESTRUCTURAL 15 MM 1220 X 2440",
+            "recommended": [
+                { "name": "TERCIADO MOLDAJE 15 MM 1220 X 2440", "score": 0.95978415 },
+                { "name": "TERCIADO ESTRUCTURAL 18 MM 1220 X 2440", "score": 0.8745111 },
+                { "name": "TERCIADO MOLDAJE 18 MM 1220 X 2440", "score": 0.8411093 },
+                { "name": "TERCIADO ESTRUCTURAL 12 MM 1220 X 2440", "score": 0.8237744 },
+                { "name": "MASISA MELAMINA LINGUE 15 X 1520 X 2440", "score": 0.7271608 },
+                { "name": "MASISA MELAMINA LENGA 15 X 1520 X 2440", "score": 0.72601295 },
+                { "name": "DUROLAC 3 MM COIGUE 1520 X 2440", "score": 0.7090162 },
+                { "name": "DUROLAC 3 MM CEDRO 1520 X 2440", "score": 0.7078656 }
+            ]
+        }
+    ]
 def landing(request):
     '''
     Main function for handling landing page operations and form submission.
@@ -22,8 +68,8 @@ def landing(request):
 
     contactForm = ContactForm
     suscriptionForm = SuscriptionForm
-
-    return render(request, 'landing.html', { 'contactForm': contactForm, 'suscriptionForm': suscriptionForm, 'countDownDateTime': datetime.datetime.now() })   
+    
+    return render(request, 'landing.html', { 'contactForm': contactForm, 'suscriptionForm': suscriptionForm, 'countDownDateTime': datetime.datetime.now(), 'recommendedProducts': recommended_products })   
 
 def thanks_suscription(request):
     if request.method == "POST":
