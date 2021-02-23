6ec2b19e5479b0d2c720b9d16e0cc50d3be93161
Vicente Escobar <60410823+vescobarb@users.noreply.github.com>
2021-02-23 20:30:22 -0300

    Post method included

--- user_panel/forms.py
+++ user_panel/forms.py
@@ -30,31 +30,14 @@ class FileSubmissionForm(forms.Form):
     This is the main form in the website. It allowes a user tu submit his details and a file.
     '''
     validate_extension = FileExtensionValidator(ALLOWED_EXTENSIONS, message=f"Extension no permitida. Por favor usar un archivo alguna de las siguientes extensiones: {', '.join(ALLOWED_EXTENSIONS)}")
-
-    name = forms.CharField(label='Name', max_length=250, required=True)
-    last_name = forms.CharField(label='Last Name', max_length=250, required=True)
-    email = forms.EmailField(label='Email', max_length=250, required=True)
-    phone = forms.CharField(label='Phone', max_length=12, required=True)
-    company = forms.CharField(label='Company', max_length=250, required=True)
-
     document = forms.FileField(label='Excel/CSV', required=False, validators=[validate_extension])
-    recieve_info_flag = forms.BooleanField(label='Suscribirme a la lista de mails', required=False)
-    
-    name.widget.attrs.update({'class' : 'form-control', 'placeholder': 'Nombre'})
-    last_name.widget.attrs.update({'class' : 'form-control', 'placeholder': 'Apellido'})
-    email.widget.attrs.update({'class' : 'form-control', 'placeholder': 'Email'})
-    phone.widget.attrs.update({'class' : 'form-control', 'placeholder': 'Telefono'})
-    company.widget.attrs.update({'class' : 'form-control', 'placeholder': 'Empresa'})
     document.widget.attrs.update({'class' : 'form-control-file'})
-    recieve_info_flag.widget.attrs.update({'class' : 'form-check'})
-
     def clean_extensions(self):
         ext = self.cleaned_data['document'].name.split('.')[-1]
         logger.info(ext)
         logger.info('validation cycle')
         if ext not in ALLOWED_EXTENSIONS:
             raise ValidationError(('Extension no permitida'), code='invalid')
-        
         return self.cleaned_data
 
 class FieldSelectionForm(forms.Form):
--- user_panel/views.py
+++ user_panel/views.py
@@ -14,36 +14,35 @@ from django.template import Context, loader
 
 class ProtectView(LoginRequiredMixin, View):
     def get(self, request):
-        if request.method == "POST":
-            form = FileSubmissionForm(request.POST, request.FILES)
-            if form.is_valid():
-                file_path, gc_url = handle_uploaded_file(request.FILES['document'], company=request.user.company, local=False)
-                stored_file = CompanyData(document_location=file_path, user=user)
-                stored_file.save()
-                email_cliente = f"""Hola, {request.user.first_name}!\n\nTu data está siendo procesada y te enviarémos un correo a penas tengamos el resultado.\n\nGracias por confiar en nosotros!\n\nEquipo de Cactus Co"""
-                email_interno = f"""Nueva data subida por {request.user.first_name} {request.user.last_name} de {request.user.company}, este es el archivo {file_path}"""
-                
-                message1 = (
-                    'Recomendacion de productos',
-                    email_cliente,  
-                    'agustin.escobar@cactusco.cl', 
-                    [email]
-                )
+        form = FileSubmissionForm
+        return render(request, 'welcome.html', { 'form':form })
+    
+    def post(self, request):
+        form = FileSubmissionForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_path, gc_url = handle_uploaded_file(request.FILES['document'], company=request.user.company, local=False)
+            stored_file = CompanyData(document_location=file_path, user=request.user)
+            stored_file.save()
+            email_cliente = f"""Hola, {request.user.first_name}!\n\nTu data está siendo procesada y te enviarémos un correo a penas tengamos el resultado.\n\nGracias por confiar en nosotros!\n\nEquipo de Cactus Co"""
+            email_interno = f"""Nueva data subida por {request.user.first_name} {request.user.last_name} de {request.user.company}, este es el archivo {file_path}"""
+            
+            message1 = (
+                'Recomendacion de productos',
+                email_cliente,  
+                'agustin.escobar@cactusco.cl', 
+                [email]
+            )
 
-                message2 = (
-                    f'Nueva data!! Cliente: {name} {last_name}, {company}',
-                    email_interno, 
-                    'agustin.escobar@cactusco.cl', 
-                    ['agustin@cactusco.cl', 'vicente@cactusco.cl', 'rodrigo@cactusco.cl']
-                )
+            message2 = (
+                f'Nueva data!! Cliente: {request.user.first_name} {request.user.last_name}, {request.user.company}',
+                email_interno, 
+                'agustin.escobar@cactusco.cl', 
+                ['agustin@cactusco.cl', 'vicente@cactusco.cl', 'rodrigo@cactusco.cl']
+            )
 
-                send_mass_mail((message1, message2), fail_silently=False)
+            send_mass_mail((message1, message2), fail_silently=False)
 
-                request.session['file_path'] = file_path
-                request.session['available_fields'] = get_available_fields(file_path)
-                request.session['user_id'] = user.id
-                return HttpResponseRedirect(reverse('field_selection'))
-        
-        else:
-            form = FileSubmissionForm
-        return render(request, 'welcome.html', { 'form':form, 'has_submitted': False })
+            request.session['file_path'] = file_path
+            request.session['available_fields'] = get_available_fields(file_path)
+            request.session['user_id'] = request.user.id
+            return HttpResponseRedirect(reverse('field_selection'))
