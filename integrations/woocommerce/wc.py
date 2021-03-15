96a2300ccf70b1149d1fa1df9bc2ef032361b989
Vicente Escobar <60410823+vescobarb@users.noreply.github.com>
2021-03-15 11:03:48 -0300

    pagination completed and added parameters to filter after first request

--- integrations/woocommerce/wc.py
+++ integrations/woocommerce/wc.py
@@ -23,48 +23,51 @@ wcapi = API(
     version="wc/v3"
 )
 
-def get_orders(wcapi=wcapi):
-    endpoint = "orders"
-    params = {
-        'per_page': 100,
-        'page': 1,
-    }
-    resp = wcapi.get(endpoint, params=params).json()
-    filename = './integrations/woocommerce/orders.json'
-    with open(filename, 'w+') as f:
-        f.write(json.dumps(resp))
-    blob_name = f"{COMPANY}/{endpoint}_{DATE}.json"
-    upload_blob_to_default_bucket(filename,blob_name)
-    os.remove(filename)
-
-def get_products(wcapi=wcapi):
-    endpoint = "products"
-    params = {
-        'per_page': 100,
-        'page': 1,
-    }
-    resp = wcapi.get(endpoint, params=params).json()
-    filename = './integrations/woocommerce/products.json'
-    with open(filename, 'w+') as f:
-        f.write(json.dumps(resp))
-    blob_name = f"{COMPANY}/{endpoint}_{DATE}.json"
-    upload_blob_to_default_bucket(filename,blob_name)
-    os.remove(filename)
+def get_orders_prod(wcapi=wcapi):
+    endpoints = ["orders", "products"]
+    for endpoint in endpoints:
+        pages = []
+        for e in range(10000):
+            if endpoint == "orders"
+                params = {
+                    'per_page': 100,
+                    'page': e+1,
+                    #'after':, #Limit response to resources published after a given ISO8601 compliant date.
+                }
+            else:
+                params = {
+                    'per_page': 100,
+                    'page': e+1,
+                    #'exclude':, #Ensure result set excludes specific IDs.
+                }
+            resp = wcapi.get(endpoint, params=params).json()
+            if resp == []:
+                break
+            else:
+                for page in resp:
+                    pages.append(page)
+        filename = f'./integrations/woocommerce/{endpoint}.json'
+        with open(filename, 'w+') as f:
+            f.write(json.dumps(pages))
+        blob_name = f"{COMPANY}/{endpoint}_{DATE}.json"
+        upload_blob_to_default_bucket(filename,blob_name)
+        #os.remove(filename)
 
 def upload_upsell(wcapi=wcapi):
+
+    id_producto = "14"
     up_sell = ["18"]
     coss_sell = ["12"]
     data = {
         "upsell_ids": up_sell,
         "cross_sell_ids": coss_sell
     }
-    id_producto = "14"
+    
     print(wcapi.put(f"products/{id_producto}", data).json())
 
 
 if __name__ == "__main__":
     if METHOD == "get_data":
-        get_orders()
-        get_products()
+        get_orders_prod()
     elif METHOD == "post_data":
         upload_upsell()
