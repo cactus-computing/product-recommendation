d95768540f434e774d159e73765273014851355f
Agustín Escobar <agustin.escobar.blanc@gmail.com>
2021-03-20 11:28:29 -0300

    hotfix: import error

--- models/views.py
+++ models/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import ModelPredictions
 
 def get_top_predictions_for(porduct_id, k=5):
     return ModelPredictions.objects.filter(porduct_id=product_id)[:k]
