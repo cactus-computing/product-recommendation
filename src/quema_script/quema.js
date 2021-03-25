692067780e40d39fe7d1aa691adbc9343825dc01
royarzuni <rodrigo.oyarzun25@gmail.com>
2021-03-25 10:18:20 -0300

    cambio de var a const

--- src/quema_script/quema.js
+++ src/quema_script/quema.js
@@ -1,23 +1,23 @@
 window.onload = addCactusRecommendation;
-var CODE_STATUS = 'local' // options: local, dev, prod
-var COMPANY = "quema";
+const CODE_STATUS = 'local' // options: local, dev, prod
+const COMPANY = "quema";
 
-var HOST_DICT = {
+const HOST_DICT = {
     local: "http://localhost:8000",
     dev: "https://dev.cactusco.cl",
     prod: "https://cactusco.cl"
 }
 
-var formatter = new Intl.NumberFormat('es-CL', {
+const formatter = new Intl.NumberFormat('es-CL', {
     style: 'currency',
     currency: 'CLP',
   })
 
 function addCactusRecommendation () {
     /* Create Div and add content */
-    var recommenderSection = document.createElement("div");
-    var link = document.createElement("link");
-    var head = document.head;
+    const recommenderSection = document.createElement("div");
+    const link = document.createElement("link");
+    const head = document.head;
     
     link.type = "text/css";
     link.rel = "stylesheet";
@@ -26,7 +26,7 @@ function addCactusRecommendation () {
     head.appendChild(link);
 
     /*Create --> <script type="text/javascript" src="js/lightslider.js"></script>*/
-    var scriptSlider = document.createElement("script");
+    const scriptSlider = document.createElement("script");
     
     scriptSlider.type = "text/javascript";
     scriptSlider.src = "slider.js";
@@ -34,42 +34,42 @@ function addCactusRecommendation () {
 
     recommenderSection.className = "cross-sell-slider";
     recommenderSection.id = "cross-sell-slider"
-    var targetDiv = document.querySelector("#main .elementor-inner");
+    const targetDiv = document.querySelector("#main .elementor-inner");
     //var targetDiv = document.querySelector("#main .elementor-inner .elementor-section-wrap");
     products = [];
 
-    var titleDiv = document.createElement("div");
+    const titleDiv = document.createElement("div");
     titleDiv.className = "cross-sell-title";
-    var sectionTitle = document.createElement("h2");
+    const sectionTitle = document.createElement("h2");
     sectionTitle.innerText = "Productos Relacionados";
     titleDiv.appendChild(sectionTitle)
     recommenderSection.appendChild(titleDiv)
 
-    var slideBoxDiv = document.createElement("div");
+    const slideBoxDiv = document.createElement("div");
     slideBoxDiv.className = "cross-sell-slide-box";
     slideBoxDiv.id = "cross-sell-slide-box";
     recommenderSection.appendChild(slideBoxDiv);
 
-        var arrowLeft = document.createElement("button");
+        const arrowLeft = document.createElement("button");
         arrowLeft.className = "ctrl-btn pro-prev";
         arrowLeft.innerText = "<";
         slideBoxDiv.appendChild(arrowLeft);
 
-        var productsDiv = document.createElement("div");
+        const productsDiv = document.createElement("div");
         productsDiv.className = "cross-sell-slide";
         productsDiv.id = "cross-sell-slide";
         slideBoxDiv.appendChild(productsDiv);
 
-        var arrowRight = document.createElement("button");
+        const arrowRight = document.createElement("button");
         arrowRight.className = "ctrl-btn pro-next";
         arrowRight.innerText = ">";
         slideBoxDiv.appendChild(arrowRight);
 
-    var productName = document.querySelector(".elementor-widget-container h1").innerText;
+    const productName = document.querySelector(".elementor-widget-container h1").innerText;
     console.log(productName)
 
-    var host = "https://cactusco.cl";
-    var localhost = "http://localhost:8000";
+    const host = "https://cactusco.cl";
+    const localhost = "http://localhost:8000";
     // fetch data from API
     fetch(
         host + "/api/cross_selling?name=" + productName+ "&company="+COMPANY+"&top-k=10"
@@ -78,25 +78,25 @@ function addCactusRecommendation () {
     }).then( function(data) {
         console.log(data["data"][0])
         data["data"].forEach( function(prod) {
-            var productDiv = document.createElement("div");
+            const productDiv = document.createElement("div");
             productDiv.id = prod['sku']
             productDiv.className = "cross-sell-product";
-                var productImageLink = document.createElement("a")
+                const productImageLink = document.createElement("a")
                 productImageLink.href = prod['permalink']
                  
-                var productImage = document.createElement("img");
+                const productImage = document.createElement("img");
                 productImage.src = prod['href']
                 productImage.className = "product-image";
                 productImageLink.appendChild(productImage)
                 productDiv.appendChild(productImageLink);
 
-                var productNameDiv = document.createElement("div");
+                const productNameDiv = document.createElement("div");
                 productNameDiv.className = "product-name-box";
 
-                    var productTitleLink = document.createElement("a")
+                    const productTitleLink = document.createElement("a")
                     productTitleLink.href = prod['permalink']
                     
-                    var productTitle = document.createElement("h2");
+                    const productTitle = document.createElement("h2");
                     productTitle.innerText = prod['name']
                     productTitle.className = "product-name";
                     productTitleLink.appendChild(productTitle)
@@ -104,10 +104,10 @@ function addCactusRecommendation () {
                     
                     productDiv.appendChild(productNameDiv)
                 
-                    var productPriceDiv = document.createElement("div");
+                    const productPriceDiv = document.createElement("div");
                 productPriceDiv.className = "product-price-box";
                     
-                    var productPrice = document.createElement("span");
+                    const productPrice = document.createElement("span");
                     productPrice.innerText = formatter.format(prod['price']);
                     productPrice.className = "product-price";
                     productPriceDiv.appendChild(productPrice);
@@ -119,7 +119,7 @@ function addCactusRecommendation () {
     });
     
 
-    var cactusContainer = document.createElement("div");
+    const cactusContainer = document.createElement("div");
     cactusContainer.id = "cactusContainer"
     cactusContainer.class = "cactusRecommendation"
 
