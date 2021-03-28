9f14f30b21575e548742d80e5a27217e948856af
Vicente Escobar <60410823+vescobarb@users.noreply.github.com>
2021-03-28 11:29:47 -0300

    perf(upload_related_prod): delete product in related models before updloading

--- scripts/product_upload.py
+++ scripts/product_upload.py
@@ -2,12 +2,11 @@ import csv  # https://docs.python.org/3/library/csv.html
 # https://django-extensions.readthedocs.io/en/latest/runscript.html
 # python3 manage.py runscript cats_load
 import logging
-from api.models import CrossSellPredictions, UpSellPredictions, ProductAttributes
-from api.serializers import CrossSellPredictionsSerializer, UpSellPredictionsSerializer, ProductAttributesSerializer
 from datetime import datetime
 from tqdm import tqdm
 from google.cloud import storage
 from google.oauth2 import service_account
+from api.models import CrossSellPredictions, UpSellPredictions, ProductAttributes
 
 DATE = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
@@ -31,26 +30,21 @@ credentials = service_account.Credentials.from_service_account_file(
 
 def run(*args):
     client_name = args[0]
-    
     client = storage.Client(credentials=credentials, project=credentials.project_id)
     bucket = client.bucket(BUCKET_NAME)
-
     blob = bucket.blob(f"{client_name}/{client_name}_products.csv")
     blob.download_to_filename(f'./scripts/data/{client_name}_products.csv')
     blob = bucket.blob(f"{client_name}/{client_name}_cross.csv")
     blob.download_to_filename(f'./scripts/data/{client_name}_cross.csv')
     blob = bucket.blob(f"{client_name}/{client_name}_up.csv")
     blob.download_to_filename(f'./scripts/data/{client_name}_up.csv')
-    
     logger.info(f"Process {client_name}")
-    
     fhand = open(f'./scripts/data/{client_name}_products.csv')
     product = csv.reader(fhand)
     fhand = open(f'./scripts/data/{client_name}_cross.csv')
     cross_sell = csv.reader(fhand)
     fhand = open(f'./scripts/data/{client_name}_up.csv')
     up_sell = csv.reader(fhand)
-
     # Subir todos los productos a ProductAttributes
     for e, row in tqdm(enumerate(product)):
         if e == 0:
@@ -75,13 +69,30 @@ def run(*args):
             )
 
     # Iterar sobre los UpSells y subirlos, relacionando el product_code con el ID
+    in_upsell = []
     for e, up in tqdm(enumerate(up_sell)):
         if e == 0:
             logger.info("Reading Up Sells. Fields:")
             logger.info(", ".join(up))
         else:
-            original_product = ProductAttributes.objects.get(product_code=up[0], company=client_name)
-            related_product = ProductAttributes.objects.get(product_code=up[1], company=client_name)
+            if up[0] not in in_upsell:
+                in_upsell.append(up[0])
+                original_product = ProductAttributes.objects.get(
+                    product_code=up[0], 
+                    company=client_name
+                    )
+                UpSellPredictions.objects.filter(
+                    product_code=original_product, 
+                    company=client_name
+                    ).delete()
+            original_product = ProductAttributes.objects.get(
+                product_code=up[0], 
+                company=client_name
+                )
+            related_product = ProductAttributes.objects.get(
+                product_code=up[1],
+                company=client_name
+                )
             us = UpSellPredictions.objects.update_or_create(
                 product_code=original_product, 
                 recommended_code=related_product,                 
@@ -90,14 +101,30 @@ def run(*args):
                     'distance': up[2],
                 }
                 )
-
+    in_cross = []
     for e, cross in tqdm(enumerate(cross_sell)):
         if e == 0:
             logger.info("Reading Cross. Fields:")
             logger.info(", ".join(cross))
         else:
-            original_product = ProductAttributes.objects.get(product_code=cross[0], company=client_name)
-            related_product = ProductAttributes.objects.get(product_code=cross[1], company=client_name)
+            if cross[0] not in in_cross:
+                in_cross.append(cross[0])
+                original_product = ProductAttributes.objects.get(
+                    product_code=cross[0], 
+                    company=client_name
+                    )
+                CrossSellPredictions.objects.filter(
+                    product_code=original_product, 
+                    company=client_name
+                    ).delete()
+            original_product = ProductAttributes.objects.get(
+                product_code=cross[0], 
+                company=client_name
+                )
+            related_product = ProductAttributes.objects.get(
+                product_code=cross[1], 
+                company=client_name
+                )
             
             cs = CrossSellPredictions.objects.update_or_create(
                 product_code=original_product, 
