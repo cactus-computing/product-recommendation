1dcf97bf0d74a576a75dc01c8814b59270e94c8f
Agustín Escobar <agustin.escobar.blanc@gmail.com>
2021-04-05 16:57:30 -0400

    fix: static files

--- cactusco/urls.py
+++ cactusco/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('api.urls'))
-    ]
+]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
