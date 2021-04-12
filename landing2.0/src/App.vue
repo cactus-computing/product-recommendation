e33840188241887efb70c47bdd3c2f0475deb2d7
royarzuni <rodrigo.oyarzun25@gmail.com>
2021-04-12 11:39:01 -0400

    feat: new landing, WIP

---
+++ landing2.0/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <Hero/>
+    <Features/>
+    <Pricing/>
+    <Cta/>
+    <Contact/>
+    <Footer/>
+</template>
+
+<script setup>
+    import Hero from './components/Hero.vue'
+    import Footer from './components/Footer.vue'
+    import Features from './components/Features.vue'
+    import Cta from './components/Cta.vue'
+    import Pricing from './components/Pricing.vue'
+    import Contact from './components/Contact.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> 
