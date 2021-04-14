f16e1ff47a924220ff990974c00d43d969f19c3d
royarzuni <rodrigo.oyarzun25@gmail.com>
2021-04-14 11:44:48 -0400

    feat: new landing trying to configure vue-router

--- landing/demos/src/main.js
+++ landing/demos/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import './assets/styles/index.css';
 import router from './router';
 
+Vue.config.devtools = true;
+
 const app = new Vue({
     el: '#app',
     components: {
--- landing/urls.py
+++ landing/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.landing, name='landing'),
     path('thanks/contact', views.thanks_contact, name='thanks_contact'),
     path('thanks/suscription', views.thanks_suscription, name='thanks_suscription'),
-    path('demos', views.Demos.as_view(), name='demos')
+    path('demos', views.Demos.as_view(), name='demos'),
+    path('demos/landing', views.Demos.as_view(), name='landing')
 ]
 
 handler404 = views.error404
