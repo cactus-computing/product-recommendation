3e14015674cbe86be430fceea52e87bb03710b03
Vicente Escobar <60410823+vescobarb@users.noreply.github.com>
2021-04-20 08:58:38 -0400

    chore(celery-config): updated files to run celery on heroku

--- cactusco/settings.py
+++ cactusco/settings.py
@@ -191,8 +191,8 @@ INTERNAL_IPS = [
 
 
 # Celery setting
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = env('REDIS_URL')
+CELERY_RESULT_BACKEND = env('REDIS_URL')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
