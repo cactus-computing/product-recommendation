6165a37a0a02573f123d6f33eda4d409ee8fd617
Agustín Escobar <agustin.escobar.blanc@gmail.com>
2021-04-21 10:29:37 -0400

    feat: updated customer models

--- store/models.py
+++ store/models.py
@@ -1,29 +1,35 @@
 from django.db import models
 from django_cryptography.fields import encrypt
 
-class Store(models.Model):
+class Stores(models.Model):
     company = models.CharField(max_length=500)
-    fare = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     published_at = models.DateTimeField(default=None, null=True)
-    store_logo_url = models.CharField(max_length=2000, null=True)
+    fare = models.IntegerField(null=True)
     gtm_id = models.CharField(max_length=500, null=True)
     ga_measurement_id = models.CharField(max_length=500, null=True)
     segment_key = models.CharField(max_length=500, null=True)
+    
+
+    def __str__(self):
+        return self.company
+
+class FrontAttributes(models.Model):
+    store_logo_url = models.CharField(max_length=2000, null=True)
     target_div = models.CharField(max_length=500, null=True)
     product_name_selector = models.CharField(max_length=500, null=True)
     insert_before = models.CharField(max_length=500, null=True)
     product_page_identifier = models.CharField(max_length=500, null=True)
     product_page_regex = models.CharField(max_length=500, null=True)
+
+class ApiAttributes(models.Model):
     consumer_key = encrypt(models.CharField(max_length=500, null=True))
     consumer_secret = encrypt(models.CharField(max_length=500, null=True))
     api_url = models.CharField(max_length=1000, null=True)
-    def __str__(self):
-        return self.company
 
 class Customers(models.Model):
-    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    store = models.ForeignKey(Stores, on_delete=models.CASCADE)
     name = models.CharField(max_length=2000, null=True)
     last_name = models.CharField(max_length=2000, null=True)
     email = models.CharField(max_length=2000, null=True)
